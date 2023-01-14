8decd508f79d54d4f5af58ee797cec33629d7ad2
Marcell O <omarcell@yahoo.com>
2023-01-14 18:51:15 +0000

    Wrote POST request for jobs. Wrote game plan

--- src/scraping/config.py
+++ src/scraping/config.py
@@ -1,1 +1,44 @@
-jooble_url = "https://hu.jooble.org/%C3%A1ll%C3%A1s/K%C3%BClf%C3%B6ld"
+jooble_get_url = "http://hu.jooble.org/%C3%A1ll%C3%A1s/K%C3%BClf%C3%B6ld"
+
+jooble_post_url = "http://hu.jooble.org/api/serp/jobs"
+
+jooble_post_headers = {
+    "Accept": "/",
+    "Connection": "keep-alive",
+    "content-type": "application/json",
+    "Host": "hu.jooble.org",
+    "Origin": "https://hu.jooble.org",
+    "Referer": jooble_get_url,
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fect-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
+}
+
+jooble_post_cookies = {
+    "SessionCookie.hu": "-7605529932450157550*2231947513189314018*638093143747064588"
+}
+
+
+def generate_post_request_json(page_num):
+    """
+    Function to generate dictionary to send to
+    Jooble in a HTTPS POST request.
+    
+    Inputs:
+    page_num - int: Page number for request
+    
+    Returns:
+    request - dict: Request headers"""
+
+    jooble_post_json = {
+        "coords": None,
+        "isCityregion": False,
+        "isRemoteSerp": False,
+        "jobTypes": [],
+        "page": page_num,
+        "region": "Külföld",
+        "regionId": 4357,
+        "search": "",
+    }
+    return jooble_post_json
--- src/scraping/jooble.py
+++ src/scraping/jooble.py
@@ -1,6 +1,25 @@
 import requests
-from config import *
+import config
+import json
 
-r = requests.get(jooble_url)
-response = r.text
+# Plan:
+# Send request to get ALL jobs - DELAY of 1 sec -> 5 min scrape
+# Collect UIDs: content["jobs"][x]["uid"]
+# Scrape URLs for new items -> get full description
+# Save all data to 3 databases (csv)
+# 1. Job descriptions - most of content["jobs"][x]
+# 2. Company databaSE - content["jobs"][x]["company"] -> Collect websites as well?
+# 3. Location - mostly country - is this necessary?
+
+# New advert - scrape individual site to get FULL job ad content
+
+response = requests.post(
+    url=config.jooble_post_url,
+    headers=config.jooble_post_headers,
+    cookies=config.jooble_post_cookies,
+    json=config.generate_post_request_json(1)
+)
+
+content = json.loads(response.text)
+total_jobs = content["count"]
 a = 1

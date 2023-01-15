734004e2f29c77875f1ee125f6936ca61e4e0515
Marcell O <omarcell@yahoo.com>
2023-01-15 13:14:47 +0000

    Wrote flatten function

--- src/common/functions.py
+++ src/common/functions.py
@@ -3,13 +3,28 @@ from collections.abc import MutableMapping
 def flatten_dict(
                 d: MutableMapping,
                 parent_key: str = '',
-                sep: str ='.') -> MutableMapping:
+                sep: str ='',
+                camelCase: bool = True) -> MutableMapping:
     """
     Function to flatten python dictionaries
+
+    Inputs:
+    d - dict: dictionary to flatten
+    parent_key - str: Key to use for top level of dictionary
+    sep - str: Separate to use between levels
+    camelCase - bool: Whether to uppercase 1st characters
+    of lower-level keys
+
+    Returns:
+    flattened_dict - dict: The flattened dictionary
     """
+
     items = []
     for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
+        # Make keys camelCase - continue example set in response keys
+        k_cased = k[0].upper() + k[1:] if camelCase else k
+        new_key = parent_key + sep + k_cased if parent_key else k
+
         if isinstance(v, MutableMapping):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:

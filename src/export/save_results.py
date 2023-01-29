4265194f5933b80732a2791430d3ef1362ec0b0f
Marcell O <omarcell@yahoo.com>
2023-01-29 16:07:19 +0000

    Wrote continuation bit

--- src/export/save_results.py
+++ src/export/save_results.py
@@ -1,6 +1,7 @@
 # Common libraries
 import logging
 import os
+import pandas as pd
 
 # Custom packages
 import common.config as cfg
@@ -27,13 +28,37 @@ def job_details(
     Returns:
     None
     """
-    filemode = "a" if append else "w"
+    # Handle writing new file
+    if not append:
+        logging.info(f"Saving {len(job_df.index)} lines to new file...")
+        job_df.to_csv(filename, mode="w", **kwargs)
+        logging.info(f"Saved all items to {filename}")
 
-    job_df.to_csv(filename, mode=filemode, **kwargs)
-    logging.info(f"Saved {len(job_df.index)} lines to {filename}")
+    # Read in old file and only add new items (remove duplicates)
+    else:
+        try:
+            existing_df = pd.read_csv(filename, **kwargs)
+        except FileNotFoundError:
+            logging.error(f"Existing data file {filename} not found.")
+        
+        # Get unique UID values
+        existing_uid = existing_df[cfg.unique_id_column_name].unique().tolist()
+        logging.info(f"Found {len(existing_uid)} unique values already saved for today")
 
+        # Only add new UIDs
+        new_uid = job_df[cfg.unique_id_column_name].unique().tolist()
 
-# TODO: Allow continuation mid-way (scraping failure)
+        # Select UIDs that are in new but not in existing
+        add_uid = list(set(new_uid) - set(existing_uid))
+        logging.info(f"Preparing to add {len(add_uid)} new values")
+
+        # Select new rows & append
+        new_rows = job_df[job_df[cfg.unique_id_column_name].isin(add_uid)]
+
+        # Write out new rows
+        new_rows.to_csv(filename, mode="a", **kwargs)
+        logging.info(f"Added {len(new_rows.index)} new values to {filename}")
+
 
 # Append full job text to saved data
 def job_full_description():

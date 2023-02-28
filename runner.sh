df83ff2e2659d9a0f0853f0885251233b091db1d
Marcell O <omarcell@yahoo.com>
2023-02-28 20:34:00 +0000

    Added shutdown for runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /home/omarci/masters/MScDisseration
-/home/omarci/.cache/pypoetry/virtualenvs/mscdisseration-IH9dP2zL-py3.8/bin/python /src/main.py
+/home/omarci/.cache/pypoetry/virtualenvs/mscdisseration-IH9dP2zL-py3.8/bin/python /home/omarci/masters/MScDisseration/src/main.py
 
+shutdown.exe /s

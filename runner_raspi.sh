a3489f522c9d952275f51f686e331e2e8486e63c
UnMorrer <omarcell@yahoo.com>
2023-03-18 15:41:44 +0000

    Added RasPi runner script

---
+++ runner_raspi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/pi/Repos/MScDisseration
+
+# Run script
+/home/pi/.cache/pypoetry/virtualenvs/mscdisseration-tRrFNwMr-py3.9/bin/python /home/pi/Repos/MScDisseration/src/main.py
+

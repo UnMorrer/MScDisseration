fa52af36f1af8bc4aa63b8b02df38b1377d56fb2
Marcell O <omarcell@yahoo.com>
2023-06-17 12:03:19 +0100

    Started working on de-duplication

--- src/eda/fullData.py
+++ src/eda/fullData.py
@@ -1,11 +1,68 @@
 # Exploratory data analysis for jobs with full data
-# - Language - googletrans module
+# - Language - deepl module
 # - Salary
 # - Country
+# - Duplicate ads with cosine similarity
 # - etc.
 
-import googletrans as gt # Docs: https://py-googletrans.readthedocs.io/en/latest/
 import pandas as pd
+import deepl
+import configparser
 
 # Read in data
-df 
+df = pd.read_csv("/home/omarci/masters/MScDisseration/data/merged_full.csv")
+config = configparser.ConfigParser()
+config.read(".env")
+APIKey = config["DeepL"]["DEEPL_API_KEY"]
+translator = deepl.Translator(APIKey)
+
+# Character number in full description
+print(f"Summary stats - number of characters in job description: \n")
+print(df.jobDescription.str.len().describe())
+# Min 25 - Max 14,542: Enormous range present BUT all below 15k character limit for translation
+
+def translate_job(
+        jobDescBatch,
+        deepLTranslator,
+        dest="EN-GB"):
+    """
+    Function to translate the job descriptions in batches
+    using the DeepL Translate API
+
+    Inputs:
+    jobDescBatch - [str]: A list of job descriptions to be translated OR
+    a single string that is translated
+    deepLTranslator - deepl.Trnaslator : An object to handle the translation.
+    dest - str: Destination language. More info here:
+    https://github.com/DeepLcom/deepl-python
+
+    Returns:
+    texts - [str]: The translated texts
+    sources - [str]: The list of source languages  
+    """
+    results = deepLTranslator.translate_text(
+        jobDescBatch,
+        target_lang=dest
+    )
+    
+    # Separate results
+    if type(results) == type([]):
+        texts = []
+        sources = []
+        for item in results:
+            texts.append(item.text)
+            sources.append(item.detected_source_lang)
+        
+        return texts, sources
+    
+    # If only 1 string is translated
+    return results.text, results.detected_source_lang
+
+# TODO: Translation is pricey...
+# For everything -> 13.8M characters ~ EUR 280
+# Can do 1st 100 characters from each advert -> Within free tier
+# Discuss options with supervisor
+
+# Focus INSTEAD - remove duplicate advertisements
+
+a = 1

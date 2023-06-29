50585091e6966b029d894074c0da5bcb00e343e2
Marcell O <omarcell@yahoo.com>
2023-06-29 18:31:41 +0100

    v1 code for translation done

--- src/eda/translation.py
+++ src/eda/translation.py
@@ -1,9 +1,19 @@
 import deep_translator as dptrans
+import pandas as pd
+import random as rnd
+import numpy as np
+
+merged_data_filepath = "/home/omarci/masters/MScDisseration/data/merged_full.csv"
+translations_filepath = "/home/omarci/masters/MScDisseration/data/translated_descriptions.csv"
 
 # TODO: Look into LOCAL translation, or at least language detection
 # Translation lib: https://pypi.org/project/deep-translator/
 # Detection (local) lib: https://towardsdatascience.com/develop-a-text-language-detector-and-translator-system-in-python-6537d6244b10
 # --> Module: cld3 - https://github.com/google/cld3
+
+df = pd.read_csv(merged_data_filepath)
+translatedDescriptions = pd.read_csv(translations_filepath, index_col=False)
+batchSize = 25
 
 def translate_job_deepl(
         jobDescBatch,
@@ -75,3 +85,59 @@ def translate_job_deeptrans(
     texts = dptrans.GoogleTranslator(source=source, target=dest).translate_batch(jobDescBatch)
     return texts
 
+
+def translate_in_batches(
+        df,
+        excludeIds,
+        batchSize=10
+):
+    """
+    Function to handle bulck of batch translation
+
+    Inputs:
+    df - pd.DataFrame: A pandas DataFrame with at least
+    2 columns - id, jobDescription. One holds a unique 
+    identifier while the other has the texts to be
+    translated.
+    excludeIDs - [int]: A list of integer identifiers
+    that are to be excluded in the sampling
+
+    Returns:
+    translatedText - pd.DataFrame: A pandas DataFrame
+    of size batchSize with 2 columns: id and translatedJobDesc
+    that each hold a unique identifier and the translated job
+    description
+    """
+
+    outdf = pd.DataFrame()
+
+    # Exclude IDs not needed
+    selectiondf = df[~df['id'].isin(excludeIds)]
+    
+    # Randomly  select batchSize number of IDs
+    selectedIds = rnd.sample(selectiondf.id.tolist(), batchSize)
+
+    selectiondf = df[df['id'].isin(selectedIds)]
+    outdf["id"] = selectiondf.id
+    outdf["translatedJobDesc"] = translate_job_deeptrans(selectiondf.jobDescription.tolist())
+    
+    return outdf
+
+
+if __name__ == "__main__":
+    loopsLeft = np.ceil((df.shape[0] - translatedDescriptions.shape[0])/batchSize)
+
+    for i in range(loopsLeft):
+        try:
+            # Translate stuff
+            returndf = translate_in_batches(df, translatedDescriptions.id.tolist(), batchSize=batchSize)
+
+            # Capture results
+            translatedDescriptions = pd.concat(translatedDescriptions, returndf)
+        except Exception as e:
+            # Save if error
+            translatedDescriptions.to_csv(translatedDescriptions)
+            print(f"Error occurred: {e}")
+
+        print(f"Translation of batch {i} done")
+        print(f"Progress: {i*batchSize}/{(df.shape[0] - translatedDescriptions.shape[0])}")

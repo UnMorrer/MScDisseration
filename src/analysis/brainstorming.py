a83b367d6bda30553811eac14fb815b7e1c1ec38
Marcell O <omarcell@yahoo.com>
2023-07-09 16:03:37 +0100

    Started brainstorming

---
+++ src/analysis/brainstorming.py
@@ -0,0 +1,6 @@
+# InfoShield clustering - OK
+# Correlates collected - OK
+
+# How can I identify significant variables in clustering?
+
+# Other than InfoShield, do we have other clustering methods available?

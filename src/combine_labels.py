a4619ad17a9824baab5f09464b93cc541411e1b0
Marcell O <omarcell@yahoo.com>
2023-08-08 21:47:05 +0100

    Created joined up labels

--- src/combine_labels.py
+++ src/combine_labels.py
@@ -69,6 +69,7 @@ lowerCaseColumns = [
     "jobHiringOrganizationName",
     "descLanguage",
     ]
+letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 labels = pd.read_excel(labels, usecols=list(labelDataTypes.keys()), na_values=["Not specified", "not specified"], dtype=labelDataTypes)
 translations = pd.read_csv(translations, usecols=(list(translationDataTypes.keys()) + ["date"]), dtype=translationDataTypes, parse_dates=["date"])
@@ -76,7 +77,7 @@ translations = pd.read_csv(translations, usecols=(list(translationDataTypes.keys
 # Adverts with multiple jobs (labels)
 # -> Exclude from analysis
 # -> BUT upload as part of the data. Use 1 (advert data) : m (labels) join
-labels["cleanId"] = labels.id.map(lambda x: x.rstrip("a b c d e f g h i j k l m n o p q r s t u v w x y z")).astype(str)
+labels["cleanId"] = labels.id.map(lambda x: x.rstrip(letters)).astype(str)
 translations.rename(columns={"id": "cleanId"}, inplace=True)
 
 joinedLabels = labels.merge(translations, on="cleanId", how="left")
@@ -92,6 +93,7 @@ joinedLabels.replace(to_replace="female and male", value="gender neutral", inpla
 publishData = joinedLabels.drop(columns=["id", "cleanId"])
 publishData.to_csv("/home/omarci/masters/MScDissertation/data/public_dataset.csv", na_rep="NA", encoding='utf-8-sig')
 
-# Drop jobs with multiple adverts
+# Drop jobs with multiple adverts - 5023 adverts remain
+joinedLabels = joinedLabels[~joinedLabels["id"].str.contains("[a-zA-Z]").fillna(False)]
 
 a = 1

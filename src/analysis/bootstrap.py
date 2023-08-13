475eee6bb774dfd4ccc7c2f947bbabf9dcc51ada
Marcell O <omarcell@yahoo.com>
2023-08-13 21:38:35 +0100

    Bootstrap file added

---
+++ src/analysis/bootstrap.py
@@ -0,0 +1,6 @@
+# TODO: Bootstrap for InfoShield
+# 100 iterations for all 4 methods for starters
+
+# Bootstrap sample
+# Compare variance/difference to original
+# AND utilize other methods (see doc) - original as "ground truth" label
